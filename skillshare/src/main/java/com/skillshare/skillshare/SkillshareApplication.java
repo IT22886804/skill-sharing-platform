d004da86401ebd9cf1bc9b1aa33d7e50bfe896c3
IT22886804 <chethanadassanayake747@gmail.com>
2025-04-26 10:53:45 +0530

    Learn Plan sharing controller files created

--- skillshare/src/main/java/com/skillshare/skillshare/SkillshareApplication.java
+++ skillshare/src/main/java/com/skillshare/skillshare/SkillshareApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SkillshareApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SkillshareApplication.class, args);
+		SpringApplication.run(SkillshareApplication.class, args);
 	}
 
 }
