7a42578447f61c49bee92ecded5a89879b5c9e9b
Chethana Dassanayake <128116344+IT22886804@users.noreply.github.com>
2025-05-07 01:45:31 +0530

    Created controller, model, service, repository files of learningProgress and LearingPlan

---
+++ skillshare/src/main/java/com/example/skillshare/repository/LearningProgressRepository.java
@@ -0,0 +1,22 @@
+package com.example.skillshare.repository;
+
+
+import com.example.skillshare.model.LearningProgress;
+import com.example.skillshare.model.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LearningProgressRepository extends MongoRepository<LearningProgress, String> {
+
+    // Get all by userId
+    List<LearningProgress> findByUserId(String userId);
+
+    // Get all ordered by creation date descending
+    List<LearningProgress> findAllByOrderByCreatedAtDesc();
+
+    List<LearningProgress> findByUserIdOrderByCreatedAtDesc(String userId);
+}
+
